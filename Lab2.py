b1b26ba2aa2573b85bbd66f77ae4b68336789f2d
Racheal Nakhabala <rachealnakhabala@Racheals-MacBook-Pro.local>
2020-02-25 21:28:22 -0800

    first commit

---
+++ Lab2.py
@@ -0,0 +1,12 @@
+
+#Twinkle, twinkle, little star,
+#How I wonder what you are!
+#Up above the world so high,
+#Like a diamond in the sky.
+
+
+
+adjective = input("Enter adjective:")
+noun = input("Enter noun")
+
+print(f"twinkle, twinkle,{adjective} star, how I {noun} what you are!")
